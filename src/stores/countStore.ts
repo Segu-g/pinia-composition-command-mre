e0ac0d27dee054c78a0bc0fafde7bdc18f359529
Segu_g <segusegment@gmail.com>
2023-12-10 02:41:31 +0900

    add store definition example

--- src/stores/countStore.ts
+++ src/stores/countStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
+import { defineState } from './pinia_helper';
 
-export const CountState = defineStore({
+const CountState = defineState({
   id: 'count/state',
   state: () => ({
     counter: 0,
@@ -8,5 +9,20 @@ export const CountState = defineStore({
 });
 
 export const useCount = defineStore('count', () => {
-  return {};
+  const { defGet, defMut, defAct } = CountState.useControllerContext();
+
+  const getCount = defGet(({ state }) => {
+    return state.counter;
+  });
+  const mutIncrement = defMut(({ state }) => {
+    state.counter += 1;
+  });
+  const actIncrement = defAct(({ commit }) => {
+    commit(mutIncrement);
+  });
+
+  return {
+    getCount,
+    actIncrement,
+  };
 });
